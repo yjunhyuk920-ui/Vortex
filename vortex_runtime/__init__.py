01937eb4a304728aa7ce3f5dbc70b8fee79f346c
yjunhyuk920-ui <yjunhyuk920@gmail.com>
2026-08-02 13:35:38 +0900

    feat: export AtlasLinear runtime types

--- vortex_runtime/__init__.py
+++ vortex_runtime/__init__.py
@@ -1,10 +1,13 @@
 """VORTEX runtime research prototype."""
 
+from .atlas_linear import AtlasStats, OnlineAtlasLinear
 from .progressive import ProgressiveLinear, CertificationResult
 from .hf_loader import HuggingFaceLayout, TensorLocator
 from .llama import LlamaConfig, StreamingLlama
 
 __all__ = [
+    "AtlasStats",
+    "OnlineAtlasLinear",
     "ProgressiveLinear",
     "CertificationResult",
     "HuggingFaceLayout",
